f97b6079050dd7d3139cf901aae23dd52e8e119b
Rudolf Forsch <rudolf.forsch@dm.de>
2016-11-28 16:57:55 +0100

    ZOE-1824: setup spring boot project because we also need a backend for some persistence

---
+++ src/test/java/de/filiadata/datahub/SelaviApplicationTests.java
@@ -0,0 +1,16 @@
+package de.filiadata.datahub;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SelaviApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
