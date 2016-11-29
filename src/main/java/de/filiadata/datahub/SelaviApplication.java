881dba250dc6086b045b289c16a30e8f8b6e9457
Rudolf Forsch <rudolf.forsch@dm.de>
2016-11-29 13:01:57 +0100

    ZOE-1824: added missing java class and some sourced to be ignored

---
+++ src/main/java/de/filiadata/datahub/SelaviApplication.java
@@ -0,0 +1,12 @@
+package de.filiadata.datahub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelaviApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelaviApplication.class, args);
+	}
+}
