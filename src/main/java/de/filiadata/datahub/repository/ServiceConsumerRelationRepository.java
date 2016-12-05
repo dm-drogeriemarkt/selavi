aa841a89f292a86b3136d59cf8cd603a5fecbd3f
Rudolf Forsch <rudolf.forsch@dm.de>
2016-12-05 15:58:26 +0100

    ZOE-1824: removed separation of nodes and relations

--- src/main/java/de/filiadata/datahub/repository/ServiceConsumerRelationRepository.java
+++
@@ -1,8 +0,0 @@
-package de.filiadata.datahub.repository;
-
-import de.filiadata.datahub.domain.ServiceConsumerRelation;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ServiceConsumerRelationRepository extends CrudRepository<ServiceConsumerRelation, String> {
-
-}
