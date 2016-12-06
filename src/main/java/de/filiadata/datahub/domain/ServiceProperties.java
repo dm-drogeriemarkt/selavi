520d06451076bd785f774521183d3fc5b667604c
Rudolf Forsch <rudolf.forsch@dm.de>
2016-12-06 14:18:56 +0100

    ZOE-1824: added constraints

--- src/main/java/de/filiadata/datahub/domain/ServiceProperties.java
+++ src/main/java/de/filiadata/datahub/domain/ServiceProperties.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import java.io.Serializable;
@@ -17,7 +18,9 @@ import java.io.Serializable;
 public class ServiceProperties implements Serializable {
 
     @Id
+    @Column(unique = true, nullable = false)
     private String id;
 
+    @Column(nullable = false)
     private String content;
 }
