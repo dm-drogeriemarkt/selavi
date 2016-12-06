d9510061471958b8916d78482141dc04f8a222fa
Sven Müller <sven.mueller@dm.de>
2016-12-06 17:05:00 +0100

    ZOE-1824 add more assertions to microserviceMindmap test

--- src/test/javascript/microserviceMindmap.spec.js
+++ src/test/javascript/microserviceMindmap.spec.js
@@ -8,12 +8,14 @@ import { MicroserviceMindmap } from '../../main/javascript/components/microservi
 
 describe('<MicroserviceMindmap/>', function () {
 
+    var networkOnSpy = sinon.spy();
+
     before(function() {
         // global is node.js' window
         global.vis = {
             DataSet: sinon.spy(),
             Network: sinon.stub().returns({
-                on: sinon.spy()
+                on: networkOnSpy
             })
         };
     });
@@ -50,7 +52,26 @@ describe('<MicroserviceMindmap/>', function () {
             }
         ]
 
-        chai.expect(global.vis.DataSet.calledWith(expectedAllNodes)).to.be.ok;
+        const expectedEdges = [
+            {
+                from: "bar-consumer",
+                to: "foo-service"
+            }
+        ]
+
+        sinon.assert.calledTwice(global.vis.DataSet);
+        sinon.assert.calledWith(global.vis.DataSet, expectedAllNodes);
+        sinon.assert.calledWith(global.vis.DataSet, expectedEdges);
+
+        sinon.assert.calledOnce(global.vis.Network);
+
+        chai.expect(global.vis.Network.args[0][2].nodes.borderWidth).to.equal(2);
+        chai.expect(global.vis.Network.args[0][2].edges.width).to.equal(2);
+        chai.expect(global.vis.Network.args[0][2].layout.randomSeed).to.equal(2);
+
+        sinon.assert.calledTwice(networkOnSpy);
+        sinon.assert.calledWith(networkOnSpy, "selectNode", sinon.match.func);
+        sinon.assert.calledWith(networkOnSpy, "oncontext"); // we did not define a handler function in this test!
     });
 });
 
