068bd1054b15f01870953dbef434bf38b8c5bcee
Sven Müller <sven.mueller@dm.de>
2016-12-08 17:25:51 +0100

    ZOE-1824 make selavi ready for ci builds & deployment to servers

--- src/main/java/de/filiadata/datahub/SelaviApplication.java
+++ src/main/java/de/filiadata/datahub/SelaviApplication.java
@@ -2,12 +2,19 @@ package de.filiadata.datahub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-public class SelaviApplication {
+public class SelaviApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SelaviApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SelaviApplication.class, args);
