0e565f2df9200e01359f0a8ce520701ded0c618b
Rudolf Forsch <rudolf.forsch@dm.de>
2016-12-12 18:24:00 +0100

    ZOE-1846: Refactoring to read microservice informations from registry

--- src/main/java/de/filiadata/datahub/repository/MicroserviceRepository.java
+++ src/main/java/de/filiadata/datahub/repository/MicroserviceRepository.java
@@ -1,26 +1,24 @@
 package de.filiadata.datahub.repository;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import de.filiadata.datahub.business.DefaultNodeContentFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.hateoas.Link;
-import org.springframework.hateoas.LinkDiscoverer;
-import org.springframework.hateoas.hal.HalLinkDiscoverer;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Collections;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 
 @Service
 public class MicroserviceRepository {
 
     // TODO: extract URL to config
-    private static final String INTERNAL_GATEWAY_URL = "https://example.com/";
-    private static final String IGNORE_NODE_NAME = "self";
-    private static final String NODE_LINKS = "_links";
-    private static final String KEY_MICROSERVICE_URL = "microservice-url";
+    private static final String REGISTRY_URL = "http://example.com/eureka/apps";
+
     private RestTemplate restTemplate;
     private DefaultNodeContentFactory defaultNodeContentFactory;
 
@@ -30,29 +28,39 @@ public class MicroserviceRepository {
         this.defaultNodeContentFactory = defaultNodeContentFactory;
     }
 
-    /**
-     * Read all services from the internal microservice gateway and return a map with the service name and its url
-     */
     public Map<String, ObjectNode> findAllServices() {
-        final ObjectNode objectNode = restTemplate.getForObject(INTERNAL_GATEWAY_URL, ObjectNode.class);
-        final Iterator<String> fieldNames = objectNode.get(NODE_LINKS).fieldNames();
+        final String nodeApplications = "applications";
+        final ResponseEntity<ObjectNode> responseEntity = requestServices();
+        final ObjectNode response = responseEntity.getBody();
+        if (!response.hasNonNull(nodeApplications)) {
+            return Collections.emptyMap();
+        }
 
-        final LinkDiscoverer linkDiscoverer = new HalLinkDiscoverer();
+        final String nodeApplication = "application";
+        final JsonNode rootNode = response.get(nodeApplications);
+        if (!rootNode.hasNonNull(nodeApplication)) {
+            return Collections.emptyMap();
+        }
+
+        final ArrayNode applicationsNode = (ArrayNode) rootNode.get(nodeApplication);
+        return createResult(applicationsNode);
+    }
+
+    private Map<String, ObjectNode> createResult(ArrayNode applicationsNode) {
+        final String nodeApplicationName = "name";
         final Map<String, ObjectNode> result = new HashMap<>();
-        fieldNames.forEachRemaining(name -> {
-            if (!IGNORE_NODE_NAME.equals(name)) {
-                final Link link = linkDiscoverer.findLinkWithRel(name, objectNode.toString());
-                final ObjectNode resultNode = createResultNode(name, link.getHref());
-                result.put(name, resultNode);
-            }
+        applicationsNode.forEach(application -> {
+            String applicationName = application.get(nodeApplicationName).textValue();
+            result.put(applicationName.toLowerCase(), defaultNodeContentFactory.create(applicationName));
         });
-
         return result;
     }
 
-    private ObjectNode createResultNode(String name, String microserviceUrl) {
-        final ObjectNode node = defaultNodeContentFactory.create(name);
-        node.put(KEY_MICROSERVICE_URL, microserviceUrl);
-        return node;
+    private ResponseEntity<ObjectNode> requestServices() {
+        final HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON_UTF8));
+
+        final HttpEntity<String> httpEntity = new HttpEntity<>("parameters", headers);
+        return restTemplate.exchange(REGISTRY_URL, HttpMethod.GET, httpEntity, ObjectNode.class);
     }
 }
