9057527709e4300c84ea4c76dd4c08a4ceaeb19b
Sven Müller <sven.mueller@dm.de>
2016-12-19 13:25:28 +0100

    ZOE-1848 add filter logic to material ui-based table

--- src/main/javascript/components/microserviceList.js
+++ src/main/javascript/components/microserviceList.js
@@ -26,22 +26,45 @@ const mapDispatchToProps = (dispatch) => {
 };
 
 class MicroserviceList extends React.Component {
+
+    _shouldFilterOut(microservice, filterString) {
+        return (filterString && microservice.label) ? (microservice.label.toLowerCase().indexOf(filterString.toLowerCase()) === -1) : false;
+    }
+
     render() {
-        var microservices = this.props.microservices.map(microservice => {
-            var selected = (this.props.selectedService === microservice.id);
-            var filterHit = (this.props.filterString && microservice.label) ? (microservice.label.toLowerCase().indexOf(this.props.filterString.toLowerCase()) != -1) : false;
+        var filterHits = [];
+        var filteredOut = [];
 
-            var filterHitClass = this.props.filterHit ? "filterHit" : "";
-            var className = `microserviceListEntry ${filterHitClass}`;
-
-            return (
-                <TableRow selected={selected} className={className} key={microservice.id}>
-                    <TableRowColumn>{microservice.id}</TableRowColumn>
-                    <TableRowColumn><a href={microservice['microservice-url']}>{microservice['microservice-url']}</a></TableRowColumn>
-                    <TableRowColumn>{microservice.isExternal + ""}</TableRowColumn>
-                </TableRow>
-            )
+        this.props.microservices.forEach(microservice => {
+            if (this._shouldFilterOut(microservice, this.props.filterString)) {
+                filteredOut.push({microservice: microservice, filteredOut: true})
+            } else {
+                filterHits.push({microservice: microservice});
+            }
         });
+
+        var microservices =
+            filterHits.concat(filteredOut)
+            .map(wrapper => {
+                var microservice = wrapper.microservice;
+
+                var selected = (this.props.selectedService === microservice.id);
+
+                var style = {};
+
+                if (wrapper.filteredOut) {
+                    style.color = "lightgrey"
+                }
+
+                return (
+                    <TableRow selected={selected} className="microserviceListEntry" key={microservice.id} style={style}>
+                        <TableRowColumn>{microservice.id}</TableRowColumn>
+                        <TableRowColumn><a
+                            href={microservice['microservice-url']}>{microservice['microservice-url']}</a></TableRowColumn>
+                        <TableRowColumn>{microservice.isExternal + ""}</TableRowColumn>
+                    </TableRow>
+                )
+            });
         return (
             <div className="microserviceList">
                 <Table onRowSelection={this.props.onSelectService.bind(this)}>
@@ -53,7 +76,7 @@ class MicroserviceList extends React.Component {
                         </TableRow>
                     </TableHeader>
                     <TableBody>
-                    {microservices}
+                        {microservices}
                     </TableBody>
                 </Table>
             </div>
