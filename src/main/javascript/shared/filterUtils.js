8085cc5de22207faee57b08d330d6c92e66eaf73
Sven Müller <sven.mueller@dm.de>
2016-12-23 15:11:34 +0100

    slack: data fields to filter for can be configured

--- src/main/javascript/shared/filterUtils.js
+++ src/main/javascript/shared/filterUtils.js
@@ -1,3 +1,57 @@
+// these are the data fields that we want to search
+const _fields = [
+    'label',
+    'team'
+]
+
 export function shouldFilterOut(microservice, filterString) {
-    return (filterString && microservice.label) ? (microservice.label.toLowerCase().indexOf(filterString.toLowerCase()) === -1) : false;
+
+    if (!filterString) {
+        // filter string empty, no need to filter anything
+        return false;
+    }
+
+    for (var i = 0; i < _fields.length; i++) {
+        if (_match(microservice[_fields[i]], filterString)) {
+            return false;
+        }
+    }
+
+    // filter string does not match any of the fields we search
+    return true;
 }
+
+export function isFilterHit(fieldName, fieldValue, filterString) {
+
+    if (!filterString) {
+        // filter string empty, no hit
+        return false;
+    }
+
+    var isSearchedField = false;
+
+    for (var i = 0; i < _fields.length; i++) {
+        if (fieldName === _fields[i]) {
+            isSearchedField = true;
+            break;
+        }
+    }
+
+    if (!isSearchedField) {
+        // fieldName is not one of the fields we want to search
+        return false;
+    }
+
+    return _match(fieldValue, filterString);
+}
+
+function _match(fieldValue, filterString) {
+
+    if (typeof fieldValue !== "string") {
+        // fieldValue is not a string, cant be a hit
+        return false;
+    }
+
+    return (fieldValue.toLowerCase().indexOf(filterString.toLowerCase()) != -1);
+}
+
--- src/test/javascript/shared/filterUtils.spec.js
+++ src/test/javascript/shared/filterUtils.spec.js
@@ -2,11 +2,23 @@ var sinon = require('sinon');
 
 import chai from 'chai';
 
-import { shouldFilterOut } from '../../../main/javascript/shared/filterUtils';
+import { shouldFilterOut, isFilterHit } from '../../../main/javascript/shared/filterUtils';
 
 describe('filterUtils', function () {
     describe('shouldFilterOut', function () {
-        it('returns false if label matches filter string', function () {
+        it('returns false if filter string is empty', function () {
+
+            const service = {
+                label: 'foobar'
+            };
+
+            const filterString = '';
+
+            const result = shouldFilterOut(service, filterString);
+
+            chai.expect(result).to.be.false;
+        });
+        it('returns false if label matches filter string, and its the only field', function () {
 
             const service = {
                 label: 'foobar'
@@ -18,7 +30,7 @@ describe('filterUtils', function () {
 
             chai.expect(result).to.be.false;
         });
-        it('returns true if label does not match filter string', function () {
+        it('returns true if label does not match filter string, and its the only field', function () {
 
             const service = {
                 label: 'foobar'
@@ -29,6 +41,108 @@ describe('filterUtils', function () {
             const result = shouldFilterOut(service, filterString);
 
             chai.expect(result).to.be.true;
+        });
+        it('returns false if team matches filter string, and its the only field', function () {
+
+            const service = {
+                team: 'team foo'
+            };
+
+            const filterString = 'foo';
+
+            const result = shouldFilterOut(service, filterString);
+
+            chai.expect(result).to.be.false;
+        });
+        it('returns true if team does not match filter string, and its the only field', function () {
+
+            const service = {
+                team: 'team bar'
+            };
+
+            const filterString = 'foo';
+
+            const result = shouldFilterOut(service, filterString);
+
+            chai.expect(result).to.be.true;
+        });
+        it('returns true if the service contains none of the searched fields', function () {
+
+            const service = {
+                id: "foobar",
+                description: "this is service foobar"
+            };
+
+            const filterString = 'foo';
+
+            const result = shouldFilterOut(service, filterString);
+
+            chai.expect(result).to.be.true;
+        });
+        it('returns true if none of the searched fields contains a string', function () {
+
+            const service = {
+                team: {
+                    teamName: 'team foo'
+                }
+            };
+
+            const filterString = 'foo';
+
+            const result = shouldFilterOut(service, filterString);
+
+            chai.expect(result).to.be.true;
+        });
+        it('throws an exception when filterString is defined, but not a string', function () {
+
+            const service = {
+                label: "foobar"
+            };
+
+            const filterString = {
+                value: 'foo'
+            };
+
+            chai.expect(function() {
+                shouldFilterOut(service, filterString)
+            }).to.throw('filterString.toLowerCase is not a function');
+        });
+    });
+
+    describe('isFilterHit', function () {
+        it('returns false if fieldName is not one of the searched fields', function () {
+            const result = isFilterHit("description");
+
+            chai.expect(result).to.be.false;
+        });
+        it('returns true if fieldValue matches filterString', function () {
+            const result = isFilterHit("label", "foobar", "foo");
+
+            chai.expect(result).to.be.true;
+        });
+        it('returns false if fieldValue does not match filterString', function () {
+            const result = isFilterHit("label", "foobar", "baz");
+
+            chai.expect(result).to.be.false;
+        });
+        it('returns false if filterString is undefined', function () {
+            const result = isFilterHit("label", "foobar", undefined);
+
+            chai.expect(result).to.be.false;
+        });
+        it('returns false if filterString is empty', function () {
+            const result = isFilterHit("label", "foobar", "");
+
+            chai.expect(result).to.be.false;
+        });
+        it('throws an exception when filterString parameter is defined, but not a string', function () {
+            const filterString = {
+                value: 'foo'
+            };
+
+            chai.expect(function() {
+                isFilterHit("label", "foobar", filterString)
+            }).to.throw('filterString.toLowerCase is not a function');
         });
     });
 });
