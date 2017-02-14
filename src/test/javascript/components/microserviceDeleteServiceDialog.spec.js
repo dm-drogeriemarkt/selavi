be6986d21af7a57891a94e375f76e29f3477153b
Sven Müller <sven.mueller@dm.de>
2017-02-14 15:41:38 +0100

    SELAVI-1 add test for delete link feature in MicroserviceDeleteServiceDialog

--- src/test/javascript/components/microserviceDeleteServiceDialog.spec.js
+++ src/test/javascript/components/microserviceDeleteServiceDialog.spec.js
@@ -7,7 +7,7 @@ import {MicroserviceDeleteServiceDialog} from "../../../main/javascript/componen
 
 describe('<MicroserviceDeleteServiceDialog/>', function () {
 
-    it('is hidden when menuMode is anything but DELETE_SERVICE', function () {
+    it('is hidden when menuMode is not DELETE_SERVICE or DELETE_LINK', function () {
         const props = createProps();
 
         const wrapper = shallow(<MicroserviceDeleteServiceDialog {...props}/>);
@@ -18,34 +18,7 @@ describe('<MicroserviceDeleteServiceDialog/>', function () {
         chai.expect(wrapper.find('Snackbar').props().open).to.equal(false);
     });
 
-    it('displays delete confirmation dialog when menuMode is DELETE_SERVICE', function () {
-        const props = createProps();
-
-        props.menuMode = 'DELETE_SERVICE';
-        props.deleteServiceId = 'foobar'
-
-        const wrapper = shallow(<MicroserviceDeleteServiceDialog {...props}/>);
-
-        chai.expect(wrapper.find('Dialog').props().open).to.equal(true);
-        chai.expect(wrapper.find('Dialog').props().title).to.equal('Confirm deletion of service with id foobar');
-        chai.expect(wrapper.find('Snackbar').props().open).to.equal(false);
-    });
-
-    it('displays error message when deleteServiceErrorMessage is set', function () {
-        const props = createProps();
-
-        props.menuMode = 'DELETE_SERVICE';
-
-        props.deleteServiceErrorMessage = 'thats an error';
-
-        const wrapper = shallow(<MicroserviceDeleteServiceDialog {...props}/>);
-
-        chai.expect(wrapper.find('Dialog').props().open).to.equal(true);
-        chai.expect(wrapper.find('Snackbar').props().open).to.equal(true);
-        chai.expect(wrapper.find('Snackbar').props().message).to.equal('thats an error');
-    });
-
-    it('does not display error message when menuMode is anything but DELETE_SERVICE', function () {
+    it('does not display error message when menuMode is not DELETE_SERVICE or DELETE_LINK', function () {
         const props = createProps();
 
         props.deleteServiceErrorMessage = 'thats an error';
@@ -54,6 +27,67 @@ describe('<MicroserviceDeleteServiceDialog/>', function () {
 
         chai.expect(wrapper.find('Dialog').props().open).to.equal(false);
         chai.expect(wrapper.find('Snackbar').props().open).to.equal(false);
+    });
+
+    describe('delete services', function() {
+
+        it('displays delete confirmation dialog when menuMode is DELETE_SERVICE', function () {
+            const props = createProps();
+
+            props.menuMode = 'DELETE_SERVICE';
+            props.deleteServiceId = 'foobar'
+
+            const wrapper = shallow(<MicroserviceDeleteServiceDialog {...props}/>);
+
+            chai.expect(wrapper.find('Dialog').props().open).to.equal(true);
+            chai.expect(wrapper.find('Dialog').props().title).to.equal('Confirm deletion of service with id foobar');
+            chai.expect(wrapper.find('Snackbar').props().open).to.equal(false);
+        });
+
+        it('displays error message when deleteServiceErrorMessage is set', function () {
+            const props = createProps();
+
+            props.menuMode = 'DELETE_SERVICE';
+
+            props.deleteServiceErrorMessage = 'thats an error';
+
+            const wrapper = shallow(<MicroserviceDeleteServiceDialog {...props}/>);
+
+            chai.expect(wrapper.find('Dialog').props().open).to.equal(true);
+            chai.expect(wrapper.find('Snackbar').props().open).to.equal(true);
+            chai.expect(wrapper.find('Snackbar').props().message).to.equal('thats an error');
+        });
+    });
+
+    describe('delete links', function() {
+
+        it('displays delete confirmation dialog when menuMode is DELETE_LINK', function () {
+            const props = createProps();
+
+            props.menuMode = 'DELETE_LINK';
+            props.deleteLinkFromId = 'foobar';
+            props.deleteLinkToId = 'baz';
+
+            const wrapper = shallow(<MicroserviceDeleteServiceDialog {...props}/>);
+
+            chai.expect(wrapper.find('Dialog').props().open).to.equal(true);
+            chai.expect(wrapper.find('Dialog').props().title).to.equal('Confirm deletion of link between services foobar and baz');
+            chai.expect(wrapper.find('Snackbar').props().open).to.equal(false);
+        });
+
+        it('displays error message when deleteServiceErrorMessage is set', function () {
+            const props = createProps();
+
+            props.menuMode = 'DELETE_LINK';
+
+            props.deleteServiceErrorMessage = 'thats a link-error';
+
+            const wrapper = shallow(<MicroserviceDeleteServiceDialog {...props}/>);
+
+            chai.expect(wrapper.find('Dialog').props().open).to.equal(true);
+            chai.expect(wrapper.find('Snackbar').props().open).to.equal(true);
+            chai.expect(wrapper.find('Snackbar').props().message).to.equal('thats a link-error');
+        });
     });
 });
 
