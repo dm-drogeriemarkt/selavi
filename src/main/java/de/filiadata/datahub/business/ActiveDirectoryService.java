984e2187334ea3673cb7dffd3b1e639d047b0da5
Sven Müller <sven.mueller@dm.de>
2017-03-29 16:46:22 +0200

    SELAVI-24 add very basic ActiveDirectoryService

---
+++ src/main/java/de/filiadata/datahub/business/ActiveDirectoryService.java
@@ -0,0 +1,32 @@
+package de.filiadata.datahub.business;
+
+import org.springframework.ldap.NamingException;
+import org.springframework.ldap.core.AttributesMapper;
+import org.springframework.ldap.core.LdapTemplate;
+import org.springframework.stereotype.Service;
+
+import javax.naming.directory.Attributes;
+import java.util.List;
+
+import static org.springframework.ldap.query.LdapQueryBuilder.query;
+
+@Service
+public class ActiveDirectoryService {
+
+    private LdapTemplate ldapTemplate;
+
+    public ActiveDirectoryService(LdapTemplate ldapTemplate) {
+        this.ldapTemplate = ldapTemplate;
+    }
+
+    public List<String> getAllPersonNames() {
+        return ldapTemplate.search(
+                query().where("objectclass").is("person"),
+                new AttributesMapper<String>() {
+                    public String mapFromAttributes(Attributes attrs)
+                            throws NamingException, javax.naming.NamingException {
+                        return (String) attrs.get("cn").get();
+                    }
+                });
+    }
+}
---
+++ src/test/java/de/filiadata/datahub/business/ActiveDirectoryServiceIntegrationTest.java
@@ -0,0 +1,23 @@
+package de.filiadata.datahub.business;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ActiveDirectoryServiceIntegrationTest {
+
+    @Autowired
+    private ActiveDirectoryService activeDirectoryService;
+
+    @Test
+    public void readAllPersons() {
+        List<String> allPersonNames = activeDirectoryService.getAllPersonNames();
+        System.out.println(allPersonNames);
+    }
+}
