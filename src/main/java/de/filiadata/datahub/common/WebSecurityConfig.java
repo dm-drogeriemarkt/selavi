dd244a773b0e1e47317d6f28692cdc8d4f3d82bd
Sven Müller <sven.mueller@dm.de>
2017-05-11 15:51:06 +0200

    SELAVI-22 use activedirectory-spring-boot-starter instead of AuthenticationManagerBuilder

--- src/main/java/de/filiadata/datahub/common/WebSecurityConfig.java
+++ src/main/java/de/filiadata/datahub/common/WebSecurityConfig.java
@@ -1,33 +1,23 @@
 package de.filiadata.datahub.common;
 
-import de.filiadata.datahub.activedirectory.business.ActiveDirectoryProperties;
-import org.springframework.context.annotation.Bean;
+import de.filiadata.auth.activedirectory.ActiveDirectoryProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
-import org.springframework.ldap.core.ContextSource;
-import org.springframework.ldap.core.support.LdapContextSource;
-import org.springframework.security.authentication.encoding.LdapShaPasswordEncoder;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.ldap.DefaultSpringSecurityContextSource;
 import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
-
-import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private ActiveDirectoryProperties activeDirectoryProperties;
-    private LdapContextSource ldapContextSource;
-
-    public WebSecurityConfig(ActiveDirectoryProperties activeDirectoryProperties, LdapContextSource ldapContextSource) {
-        this.activeDirectoryProperties = activeDirectoryProperties;
-        this.ldapContextSource = ldapContextSource;
-    }
+    @Autowired
+    @ActiveDirectoryProvider
+    private AuthenticationProvider provider;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -50,17 +40,5 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
             .csrf()
                 .disable();
-    }
-
-    @Override
-    public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-            .ldapAuthentication()
-                .userSearchFilter("(&(sAMAccountName={0})(objectClass=User))")
-                .groupSearchBase("ou=Users,ou=DE,ou=dm,dc=dm,dc=int")
-                .contextSource(ldapContextSource)
-                .passwordCompare()
-                .passwordEncoder(new LdapShaPasswordEncoder())
-                .passwordAttribute("userPassword");
     }
 }
