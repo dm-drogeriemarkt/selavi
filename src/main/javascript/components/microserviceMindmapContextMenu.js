abab67f8468d3a528d2e98bc11a6f8da2be77d66
Sven Müller <sven.mueller@dm.de>
2017-06-07 16:13:41 +0200

    SELAVI-38 only display context menu items that are acutally usable (eg, edit service is only displayed when a user is logged in)

--- src/main/javascript/components/microserviceMindmapContextMenu.js
+++ src/main/javascript/components/microserviceMindmapContextMenu.js
@@ -9,7 +9,8 @@ const mapStateToProps = (state) => {
         left: state.contextMenuLeft,
         contextMenuServiceId: state.contextMenuServiceId,
         contextMenuFromId: state.contextMenuFromId,
-        contextMenuToId: state.contextMenuToId
+        contextMenuToId: state.contextMenuToId,
+        loggedInUser: state.loggedInUser
     };
 };
 
@@ -27,24 +28,31 @@ export class MicroserviceMindmapContextMenu extends React.Component {
         const { top, left } = this.props;
         const style = {position: 'fixed', top, left, zIndex: 999};
 
-        if (this.props.contextMenuServiceId) {
+        if (this.props.loggedInUser) {
+            if (this.props.contextMenuServiceId) {
+                return (
+                    <nav style={style} className="contextMenu">
+                        <button onClick={this.props.onAddProperty}>Edit Service</button>
+                        <button onClick={this.props.onDeleteService}>Delete Service</button>
+                    </nav>
+                );
+            } else if (this.props.contextMenuFromId && this.props.contextMenuToId) {
+                return (
+                    <nav style={style} className="contextMenu">
+                        <button onClick={this.props.onDeleteLink}>Delete Link</button>
+                        <button onClick={this.props.onEditLink}>Edit Link</button>
+                    </nav>
+                );
+            }
+        } else if (this.props.contextMenuServiceId) {
             return (
                 <nav style={style} className="contextMenu">
-                    <button onClick={this.props.onAddProperty}>Edit Service</button>
-                    <button onClick={this.props.onDeleteService}>Delete Service</button>
+                    <button onClick={this.props.onAddProperty}>Show Service</button>
                 </nav>
             );
-        } else if (this.props.contextMenuFromId && this.props.contextMenuToId) {
-            return (
-                <nav style={style} className="contextMenu">
-                    <button onClick={this.props.onDeleteLink}>Delete Link</button>
-                    <button onClick={this.props.onEditLink}>Edit Link</button>
-                </nav>
-            );
-        } else {
-            return <nav hidden className="contextMenu"></nav>;
         }
 
+        return <nav hidden className="contextMenu"></nav>;
     }
 }
 
--- src/test/javascript/components/microserviceMindmapContextMenu.spec.js
+++ src/test/javascript/components/microserviceMindmapContextMenu.spec.js
@@ -15,76 +15,122 @@ describe('<MicroserviceMindmapContextMenu/>', function () {
         chai.expect(wrapper.find('nav').props().hidden).to.equal(true);
     });
 
-    describe('for services', function () {
-        it('displays Edit Service / Delete Service buttons when contextMenuServiceId is set', function () {
-            const props = createProps();
-            props.contextMenuServiceId = 1337;
+    describe('when a user is logged in', function () {
+        describe('for services', function () {
+            it('displays Edit Service / Delete Service buttons when contextMenuServiceId is set', function () {
+                const props = createProps();
+                props.loggedInUser = 'my_user_id';
+                props.contextMenuServiceId = 1337;
 
-            const wrapper = shallow(<MicroserviceMindmapContextMenu {...props}/>);
+                const wrapper = shallow(<MicroserviceMindmapContextMenu {...props}/>);
 
-            chai.expect(wrapper.find('nav').props().hidden).to.be.undefined;
-            chai.expect(wrapper.find('button').at(0).text()).to.equal('Edit Service');
-            chai.expect(wrapper.find('button').at(1).text()).to.equal('Delete Service');
+                chai.expect(wrapper.find('nav').props().hidden).to.be.undefined;
+                chai.expect(wrapper.find('button').at(0).text()).to.equal('Edit Service');
+                chai.expect(wrapper.find('button').at(1).text()).to.equal('Delete Service');
+            });
+
+            it('calls onAddProperty action when Edit Service button is clicked', function () {
+                const props = createProps();
+                props.loggedInUser = 'my_user_id';
+                props.contextMenuServiceId = 1337;
+
+                const wrapper = shallow(<MicroserviceMindmapContextMenu {...props}/>);
+
+                wrapper.find('button').at(0).simulate('click');
+
+                sinon.assert.calledOnce(props.onAddProperty);
+            });
+
+            it('calls onDeleteService action when Edit Service button is clicked', function () {
+                const props = createProps();
+                props.loggedInUser = 'my_user_id';
+                props.contextMenuServiceId = 1337;
+
+                const wrapper = shallow(<MicroserviceMindmapContextMenu {...props}/>);
+
+                wrapper.find('button').at(1).simulate('click');
+
+                sinon.assert.calledOnce(props.onDeleteService);
+            });
         });
 
-        it('calls onAddProperty action when Edit Service button is clicked', function () {
-            const props = createProps();
-            props.contextMenuServiceId = 1337;
+        describe('for relations', function () {
+            it('displays Edit Link / Delete Link buttons when contextMenuFromId & contextMenuToId are set', function () {
+                const props = createProps();
+                props.loggedInUser = 'my_user_id';
+                props.contextMenuFromId = 42;
+                props.contextMenuToId = 43;
 
-            const wrapper = shallow(<MicroserviceMindmapContextMenu {...props}/>);
+                const wrapper = shallow(<MicroserviceMindmapContextMenu {...props}/>);
 
-            wrapper.find('button').at(0).simulate('click');
+                chai.expect(wrapper.find('nav').props().hidden).to.be.undefined;
+                chai.expect(wrapper.find('button').at(0).text()).to.equal('Delete Link');
+                chai.expect(wrapper.find('button').at(1).text()).to.equal('Edit Link');
+            });
 
-            sinon.assert.calledOnce(props.onAddProperty);
-        });
+            it('calls onDeleteLink action when Edit Link button is clicked', function () {
+                const props = createProps();
+                props.loggedInUser = 'my_user_id';
+                props.contextMenuFromId = 42;
+                props.contextMenuToId = 43;
 
-        it('calls onDeleteService action when Edit Service button is clicked', function () {
-            const props = createProps();
-            props.contextMenuServiceId = 1337;
+                const wrapper = shallow(<MicroserviceMindmapContextMenu {...props}/>);
 
-            const wrapper = shallow(<MicroserviceMindmapContextMenu {...props}/>);
+                wrapper.find('button').at(0).simulate('click');
 
-            wrapper.find('button').at(1).simulate('click');
+                sinon.assert.calledOnce(props.onDeleteLink);
+            });
 
-            sinon.assert.calledOnce(props.onDeleteService);
+            it('calls onEditLink action when Edit Link button is clicked', function () {
+                const props = createProps();
+                props.loggedInUser = 'my_user_id';
+                props.contextMenuFromId = 42;
+                props.contextMenuToId = 43;
+
+                const wrapper = shallow(<MicroserviceMindmapContextMenu {...props}/>);
+
+                wrapper.find('button').at(1).simulate('click');
+
+                sinon.assert.calledOnce(props.onEditLink);
+            });
         });
     });
 
-    describe('for relations', function () {
-        it('displays Edit Link / Delete Link buttons when contextMenuFromId & contextMenuToId are set', function () {
-            const props = createProps();
-            props.contextMenuFromId = 42;
-            props.contextMenuToId = 43;
+    describe('when no user is logged in', function () {
+        describe('for services', function () {
+            it('displays Show Service buttons when contextMenuServiceId is set', function () {
+                const props = createProps();
+                props.contextMenuServiceId = 1337;
 
-            const wrapper = shallow(<MicroserviceMindmapContextMenu {...props}/>);
+                const wrapper = shallow(<MicroserviceMindmapContextMenu {...props}/>);
 
-            chai.expect(wrapper.find('nav').props().hidden).to.be.undefined;
-            chai.expect(wrapper.find('button').at(0).text()).to.equal('Delete Link');
-            chai.expect(wrapper.find('button').at(1).text()).to.equal('Edit Link');
+                chai.expect(wrapper.find('nav').props().hidden).to.be.undefined;
+                chai.expect(wrapper.find('button').at(0).text()).to.equal('Show Service');
+            });
+
+            it('calls onAddProperty action when Show Service button is clicked', function () {
+                const props = createProps();
+                props.contextMenuServiceId = 1337;
+
+                const wrapper = shallow(<MicroserviceMindmapContextMenu {...props}/>);
+
+                wrapper.find('button').at(0).simulate('click');
+
+                sinon.assert.calledOnce(props.onAddProperty);
+            });
         });
 
-        it('calls onDeleteLink action when Edit Link button is clicked', function () {
-            const props = createProps();
-            props.contextMenuFromId = 42;
-            props.contextMenuToId = 43;
+        describe('for relations', function () {
+            it('displays nothing at all when contextMenuFromId & contextMenuToId are set', function () {
+                const props = createProps();
+                props.contextMenuFromId = 42;
+                props.contextMenuToId = 43;
 
-            const wrapper = shallow(<MicroserviceMindmapContextMenu {...props}/>);
+                const wrapper = shallow(<MicroserviceMindmapContextMenu {...props}/>);
 
-            wrapper.find('button').at(0).simulate('click');
-
-            sinon.assert.calledOnce(props.onDeleteLink);
-        });
-
-        it('calls onEditLink action when Edit Link button is clicked', function () {
-            const props = createProps();
-            props.contextMenuFromId = 42;
-            props.contextMenuToId = 43;
-
-            const wrapper = shallow(<MicroserviceMindmapContextMenu {...props}/>);
-
-            wrapper.find('button').at(1).simulate('click');
-
-            sinon.assert.calledOnce(props.onEditLink);
+                chai.expect(wrapper.find('nav').props().hidden).to.be.true;
+                chai.expect(wrapper.find('button').length).to.equal(0);
+            });
         });
     });
 });
@@ -97,7 +143,8 @@ function createProps() {
         onAddProperty: sinon.spy(),
         onDeleteService: sinon.spy(),
         onDeleteLink: sinon.spy(),
-        onEditLink: sinon.spy()
+        onEditLink: sinon.spy(),
+        loggedInUser: undefined
     };
 
     return props;
