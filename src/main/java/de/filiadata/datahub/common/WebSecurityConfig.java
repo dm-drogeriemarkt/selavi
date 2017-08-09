f4d167ef13e665811e334d0ec3f7779d081c68b5
Sven Müller <sven.mueller@dm.de>
2017-08-09 14:38:05 +0200

    SELAVI-59 refactor web security config: use AuthenticationManagerBuilder to configure authentication provider (instead of AuthenticationProvider bean)

--- src/main/java/de/filiadata/datahub/common/WebSecurityConfig.java
+++ src/main/java/de/filiadata/datahub/common/WebSecurityConfig.java
@@ -3,12 +3,10 @@ package de.filiadata.datahub.common;
 import de.filiadata.auth.activedirectory.cache.CachingAuthenticationProvider;
 import de.filiadata.datahub.activedirectory.business.ActiveDirectoryProperties;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -63,13 +61,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED);
     }
 
-    @Bean
-    @Order(Ordered.HIGHEST_PRECEDENCE)
-    public CachingAuthenticationProvider activeDirectoryLdapAuthenticationProvider() {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         Hotfix3960ActiveDirectoryLdapAuthenticationProvider provider;
         provider = new Hotfix3960ActiveDirectoryLdapAuthenticationProvider(properties.getDomain(), properties.getUrl(), properties.getBase());
         provider.setSearchFilter("(&(objectClass=user)(samAccountName={1}))");
         provider.setAuthoritiesMapper(authoritiesMapper);
-        return new CachingAuthenticationProvider(provider);
+        auth.authenticationProvider(new CachingAuthenticationProvider(provider));
     }
 }
