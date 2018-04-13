59c01434f21baf3c3cd143c621e93b75d53f6b45
Siebner, Bernhard <bernhard.siebner@dm.de>
2018-04-13 14:52:13 +0200

    Correct typo, use constructor iso field injection.

--- src/main/java/de/dm/common/ActiveDirectoryAuthenticationProviderConfig.java
+++ src/main/java/de/dm/common/ActiveDirectoryAuthenticationProviderConfig.java
@@ -2,35 +2,31 @@ package de.dm.common;
 
 import de.dm.activedirectory.business.ActiveDirectoryProperties;
 import de.dm.auth.activedirectory.cache.CachingAuthenticationProvider;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.ldap.authentication.ad.Hotfix3960ActiveDirectoryLdapAuthenticationProvider;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-
-import java.util.Properties;
 
 @Configuration
-@Profile({"active-direcotry"})
-public class ActiveDirecotryAuthenticationProviderConfig {
+@Profile({"active-directory"})
+public class ActiveDirectoryAuthenticationProviderConfig {
 
-	@Autowired
-	private GrantedAuthoritiesMapper authoritiesMapper;
+    private final GrantedAuthoritiesMapper authoritiesMapper;
+    private final ActiveDirectoryProperties properties;
+    private final String userRole;
 
-	@Autowired
-	private ActiveDirectoryProperties properties;
+    public ActiveDirectoryAuthenticationProviderConfig(GrantedAuthoritiesMapper authoritiesMapper,
+                                                       ActiveDirectoryProperties properties,
+                                                       @Value("${selavi.security.userRole}") String userRole) {
+        this.authoritiesMapper = authoritiesMapper;
+        this.properties = properties;
+        this.userRole = userRole;
+    }
 
-	@Value("${selavi.security.userRole}")
-	private String userRole;
-
-	@Bean
+    @Bean
 	public AuthenticationProvider adAuthenticationProvider() {
 		Hotfix3960ActiveDirectoryLdapAuthenticationProvider provider;
 		provider = new Hotfix3960ActiveDirectoryLdapAuthenticationProvider(properties.getDomain(), properties.getUrl(), properties.getBase());
