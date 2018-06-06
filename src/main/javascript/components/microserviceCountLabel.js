79a7b77e57c98126f09bc361d5d39ad958461429
Sven Müller <sven.mueller@dm.de>
2018-06-06 10:38:13 +0200

    SELAVI-16 show count of hidden nodes

--- src/main/javascript/components/microserviceCountLabel.js
+++ src/main/javascript/components/microserviceCountLabel.js
@@ -5,7 +5,8 @@ import { hasAllRequiredProperties } from './../shared/requiredPropertyUtil';
 
 const mapStateToProps = (state) => {
     return {
-        microservices: state.microservices
+        microservices: state.microservices,
+        hiddenMicroServices: state.hiddenMicroServices
     };
 };
 
@@ -20,10 +21,13 @@ export class MicroserviceCountLabel extends React.Component {
         const servicesMissingReqPropsCount = this.props.microservices.filter((microservice) => !hasAllRequiredProperties(microservice, this.props.serviceRequiredProperties)).length;
         const servicesMissingReqPropsLabel = (servicesMissingReqPropsCount === 1 ? "service" : "services") + " missing req. props";
 
+        const hiddenServicesLabel = this.props.hiddenMicroServices.length > 0 ? " | " + this.props.hiddenMicroServices.length + " services hidden" : "";
+
         return (
           <span style={{ color: "rgba(0, 0, 0, 0.4)", zIndex: 999, position: 'absolute', left: '0.5em', bottom: '0.5em' }}>
             {internalCount} {internalLabel} <span style={{ color: "#19c786" }}>&#x0272A;</span> | {externalCount} external <span style={{ color: "#f69805" }}>&#x0272A;</span>
             {servicesMissingReqPropsCount > 0 && <span> | <span style={{ textShadow: "0 0 4px #e50f03" }}>{servicesMissingReqPropsCount} {servicesMissingReqPropsLabel}</span></span>}
+            {hiddenServicesLabel}
           </span>
         );
     }
