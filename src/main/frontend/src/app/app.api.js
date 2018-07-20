d8296b29404510daba8cac976d9e07cfed2b1449
Jodiss, Paul <Paul.Jodiss@dm.de>
2018-07-20 17:23:58 +0200

    removes obsolete login function from app

--- src/main/frontend/src/app/app.api.js
+++ src/main/frontend/src/app/app.api.js
@@ -7,13 +7,6 @@ export const fetchAvailableStagesApi = async () => fetch('/selavi/services/stage
   throw new Error(`${response.statusText}: ${response.status}`);
 });
 
-export const loginApi = async () => fetch('selavi/user', { credentials: 'same-origin', headers: {} }).then(response => {
-  if (response.status === 200) {
-    return response.json();
-  }
-  throw new Error(`${response.statusText}: ${response.status}`);
-});
-
 export const fetchMicroservicesApi = async (stage) => fetch(`/selavi/services/${stage}`, { credentials: 'same-origin', headers: {} }).then(response => {
   if (response.status === 200) {
     return response.json();
