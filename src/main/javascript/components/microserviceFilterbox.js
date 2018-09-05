dec965285b979420eccdd8c0e7822d3c0b485990
Arnold Franke <arnold.franke@dm.de>
2018-09-05 17:54:43 +0200

    test heroku and onprem deploy chain

--- src/main/javascript/components/microserviceFilterbox.js
+++ src/main/javascript/components/microserviceFilterbox.js
@@ -123,7 +123,7 @@ export class MicroserviceFilterbox extends React.Component {
                     <ToolbarTitle text="SeLaVi - Service Landscape Visualizer"/>
                 </ToolbarGroup>
                 <ToolbarGroup>
-                    <TextField hintText="Filter services... (label, tags)" value={this.props.filterString}
+                    <TextField hintText="Filter services.... (label, tags)" value={this.props.filterString}
                                onChange={this.props.onType.bind(this)}/>
                 </ToolbarGroup>
                 <ToolbarGroup>
